e7125a40df73366256fa192ca1dd6b5d3f7cbffc
Швец Илья Юрьевич <mixternnik@gmail.com>
2020-06-01 13:21:07 +0600

    cleanup and movable window

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     let win = new BrowserWindow({
-        width: 960,
-        height: 540,
+        width: 1280,
+        height: 720,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        fullscreenable:true,
+        frame:false
     })
 
     win.loadFile('index.html');
